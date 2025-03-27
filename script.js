f9ebc1d304565af14b900b7a587fbb91900c14dc
samuelpeoples <peoples.sam@gmail.com>
2025-03-27 23:55:57 +1100

    Added button to delete book, added button to delete books and added dialog to add books with functinonality, still ugly as sin

--- script.js
+++ script.js
@@ -1,27 +1,88 @@
-const library = []
+const library = [];
 
-function Book(title, author, pages, genre){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.genre = genre;
-    this.id = crypto.randomUUID();
-    this.Info = function() {
-        return `${title} by ${author} is ${pages} pages long and is ${genre}`;
-    }
+function Book(title, author, pages, genre) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.genre = genre;
+	this.id = crypto.randomUUID();
+	this.Info = function () {
+		return `${title} by ${author}, ${pages} pages, ${genre}`;
+	};
 }
 
-function AddToLibrary(book){
-    library.push(book);
+function addToLibrary(book) {
+	library.push(book);
 }
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 100, "Fantasy" );
 
-const obernewtyn = new Book("Obernewtyn", "Isobelle Carmody", 200, "Fantasy", "Unkown");
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 100, "Fantasy");
 
+const obernewtyn = new Book(
+	"Obernewtyn",
+	"Isobelle Carmody",
+	200,
+	"Fantasy",
+	"Unkown"
+);
 
-AddToLibrary(theHobbit);
-AddToLibrary(obernewtyn);
+function displayUpdate() {
+	libraryDisplay.innerHTML = "";
+	let bookNum = 1;
+	library.forEach((book) => {
+		const index = bookNum - 1;
+		const bookDisplay = document.createElement("p");
+		bookDisplay.textContent = `${bookNum}: ${book.Info()}`;
+		libraryDisplay.appendChild(bookDisplay);
 
-console.log(library[0].id);
-console.log(library[1].id);
-console.log(library[1].title);
+		const deleteButton = document.createElement("button");
+		deleteButton.textContent = "delete book?";
+		bookDisplay.appendChild(deleteButton);
+
+		deleteButton.addEventListener("click", () => {
+			libraryDisplay.removeChild(bookDisplay);
+			library.splice(index, 1);
+			console.log(library);
+			displayUpdate();
+		});
+		bookNum++;
+	});
+}
+
+addToLibrary(theHobbit);
+addToLibrary(obernewtyn);
+
+const libraryDisplay = document.querySelector(".library-display");
+
+const dialog = document.querySelector("dialog");
+
+const addBookButton = document
+	.querySelector("#add-book")
+	.addEventListener("click", () => {
+		dialog.showModal();
+	});
+
+const submitBookButton = document
+	.querySelector("#submit-book")
+	.addEventListener("click", (e) => {
+		dialog.close();
+		const newBook = new Book(
+			document.querySelector("#book-title").value,
+			document.querySelector("#book-author").value,
+			document.querySelector("#book-pages").value,
+			document.querySelector("#book-genre").value
+		);
+		addToLibrary(newBook);
+		displayUpdate();
+
+        const form = document.querySelector('form');
+        form.reset();
+		e.preventDefault();
+	});
+
+const cancelBook = document.querySelector("#cancel-book").addEventListener('click',()=>{
+    const form = document.querySelector('form');
+    form.reset();
+    dialog.close();
+})
+
+displayUpdate();
